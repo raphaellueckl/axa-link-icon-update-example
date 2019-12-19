1b45359cb518c7319219ae55ec3d45cbeca4ae23
raphaellueckl <raphael.lueckl@axa-winterthur.ch>
2019-12-19 16:48:56 +0100

    Create showcase for link issue

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  // AXAButton,
+  // AXAButtonLink,
+  // AXACarousel,
+  // AXACheckbox,
+  // AXAFieldSet,
+  // AXARadio,
+  // AXAIcon,
+  // AXAInputFile,
+  // AXAInputText,
+  AXALink
+  // AXAText,
+  // AXATextarea,
+  // AXACookieDisclaimer,
+  // AXADatepicker,
+  // AXADropdown,
+  // AXAFooterSmall,
+  // AXAPolicyFeatures,
+  // AXAPolicyFeaturesItem,
+  // AXAFileUpload,
+  // // AXAPopup,
+  // AXATopContentBar,
+  // AXACommercialHeroBanner,
+  // AXAContainer,
+  // AXAFooter,
+  // AXATable,
+  // AXATableSortable,
+  // AXATestimonials
+} from "./plib-importer";
+
+class App extends React.Component {
+  state = { icon: "document" };
+
+  setIcon = newIcon => {
+    this.setState({ icon: newIcon });
+  };
+
+  // setTimeout(() => {
+  // icon = "document";
+  // }, 4000);
+
+  text = "This is hello world";
+
+  render() {
+    return (
+      <AXALink
+        href="https://axa.ch/en/private-customers.html"
+        variant="icon"
+        icon={this.state.icon}
+        onClick={() => {
+          this.setIcon("download");
+        }}
+      >
+        {this.text}
+      </AXALink>
+    );
+  }
 }
 
 export default App;
---
+++ src/plib-importer.js
@@ -0,0 +1,63 @@
+import { createElement } from "react";
+
+// import create from "@axa-ch/patterns-library-polyfill/dist/index.js";
+// import createButton from "@axa-ch/button/lib/index.react";
+// import createButtonLink from "@axa-ch/button-link/lib/index.react";
+// import createCarousel from "@axa-ch/carousel/lib/index.react";
+// import createCheckbox from "@axa-ch/checkbox/lib/index.react";
+// import createFieldset from "@axa-ch/fieldset/lib/index.react";
+// import createRadio from "@axa-ch/radio/lib/index.react";
+// import createIcon from "@axa-ch/icon/lib/index.react";
+// import createInputFile from "@axa-ch/input-file/lib/index.react";
+// import createInputText from "@axa-ch/input-text/lib/index.react";
+import createLink from "@axa-ch/link/lib/index.react";
+// import createText from "@axa-ch/text/lib/index.react";
+// import createTextarea from "@axa-ch/textarea/lib/index.react";
+// import createCookieDisclaimer from "@axa-ch/cookie-disclaimer/lib/index.react";
+// import createDatepicker from "@axa-ch/datepicker/lib/index.react";
+// import createDropdown from "@axa-ch/dropdown/lib/index.react";
+// import createFooterSmall from "@axa-ch/footer-small/lib/index.react";
+// import createPolicyFeatures, {
+//   createAXAPolicyFeaturesItemReact
+// } from "@axa-ch/policy-features/lib/index.react";
+// import createFileUpload from "@axa-ch/file-upload/lib/index.react";
+// // import createPopup from "@axa-ch/popup/lib/index";
+// import createTopContentBar from "@axa-ch/top-content-bar/lib/index.react";
+// import createCommercialHeroBanner from "@axa-ch/commercial-hero-banner/lib/index.react";
+// import createContainer from "@axa-ch/container/lib/index.react";
+// import createFooter from "@axa-ch/footer/lib/index.react";
+// import createTable from "@axa-ch/table/lib/index.react";
+// import createTableSortable from "@axa-ch/table-sortable/lib/index.react";
+// import createTestimonials from "@axa-ch/testimonials/lib/index.react";
+
+// export const AXAButton = createButton(createElement);
+// export const AXAButtonLink = createButtonLink(createElement);
+// export const AXACarousel = createCarousel(createElement);
+// export const AXACheckbox = createCheckbox(createElement);
+// export const AXAFieldSet = createFieldset(createElement);
+// export const AXARadio = createRadio(createElement);
+// export const AXAIcon = createIcon(createElement);
+// export const AXAInputFile = createInputFile(createElement);
+// export const AXAInputText = createInputText(createElement);
+export const AXALink = createLink(createElement);
+// export const AXAText = createText(createElement);
+// export const AXATextarea = createTextarea(createElement);
+// export const AXACookieDisclaimer = createCookieDisclaimer(createElement);
+// export const AXADatepicker = createDatepicker(createElement);
+// export const AXADropdown = createDropdown(createElement);
+// export const AXAFooterSmall = createFooterSmall(createElement);
+// export const AXAPolicyFeatures = createPolicyFeatures(createElement);
+// export const AXAPolicyFeaturesItem = createAXAPolicyFeaturesItemReact(
+//   createElement
+// );
+// export const AXAFileUpload = createFileUpload(createElement);
+// // export const AXAPopup = createPopup(createElement);
+// export const AXATopContentBar = createTopContentBar(createElement);
+// export const AXACommercialHeroBanner = createCommercialHeroBanner(
+//   createElement
+// );
+// export const AXAContainer = createContainer(createElement);
+// export const AXAFooter = createFooter(createElement);
+// export const AXATable = createTable(createElement);
+// export const AXATableSortable = createTableSortable(createElement);
+// export const AXATestimonials = createTestimonials(createElement);
